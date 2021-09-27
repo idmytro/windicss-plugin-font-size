614afc2c92f65adb48a60493868e8b3536b3024b
Dmytro Cisar <dmytro.cisar@gmail.com>
2021-09-27 17:07:40 +0300

    done

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
 # windicss-plugin-font-size
 
+`class="font-size-0"` `->` `.font-size-0 { font-size: 0; }`
+
+`class="font-size-16px"` `->` `.font-size-16px { font-size: 16px; }`
+
+`class="font-size-80%"` `->` `.font-size-80\% { font-size: 80%; }`
+
+`class="font-size-1.5rem"` `->` `.font-size-1\.5rem { font-size: 1.5rem; }`
+
+`class="font-size-$my-var"` `->` `.font-size-\$-my-var { font-size: var(--my-var); }`
+
+`class="font-size-large"` `->` `.font-size-large { font-size: large; }`
+
+`class="font-size-inherit"` `->` `.font-size-inherit { font-size: inherit; }`
+
+## Usage
+
+```bash
+npm i https://github.com/idmytro/windicss-plugin-font-size.git -D
+```
+
+```js
+// windi.config.js
+
+module.exports = {
+  plugins: [
+    require('windicss-plugin-font-size')
+  ]
+}
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+const plugin = require('windicss/plugin')
+const {generateFontSize} = require('windicss/utils')
+
+function isNumberLead(i) {
+  return /^\d/.test(i) ? i : undefined;
+}
+
+function isNumber(value) {
+  const n = parseFloat(value);
+  return typeof n === 'number' && isFinite(n) && value === n.toString();
+}
+
+const globalValues = ['inherit', 'initial', 'unset']
+const stringValues = ['smaller', 'xx-small', 'x-small', 'small', 'medium', 'large', 'x-large', 'xx-large', 'larger']
+
+module.exports = plugin(function ({ addDynamic, theme }) {
+  addDynamic('font', ({ Utility, Style, Property }) => {
+    if (Utility.body.indexOf('size-') === -1) return;
+
+    const amount = Utility.amount;
+    const fontSizes = theme('fontSize');
+
+    if (Object.keys(fontSizes).includes(amount)) {
+      return new Style(Utility.class, generateFontSize(fontSizes[amount])[0])
+    }
+
+    let value = Utility.handler
+      .handleSquareBrackets(isNumberLead)
+      .handleNxl((number) => `${number}rem`)
+      .handleSize()
+      .value;
+
+    if (amount.endsWith('%') && isNumber(amount.slice(0, -1))) value = amount;
+
+    if (amount === '0' || globalValues.includes(amount) || stringValues.includes(amount)) value = amount;
+
+    if (Utility.raw.startsWith('font-size-$')) value = Utility.handler.handleVariable().value;
+
+    if (value) return new Style(Utility.class, [ new Property('font-size', value) ]);
+  });
+});
